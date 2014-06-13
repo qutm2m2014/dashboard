6d6b89a608235b76cd25043d9b330fe89a04ad80
Matthew Brown <me@matthewbrown.io>
2014-06-13 14:52:07 +1000

    New charts using d3

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
-from .config import DevConfig, ProductionConfig
+from config import DevConfig, ProductionConfig
 import os
 
 
 def configure_app(app):
-    if os.environ.get("APP_ENV") is "PRODUCTION":
+    if os.environ.get("DASHBOARD_ENV") == "production":
         app.config.from_object(ProductionConfig)
     else:
         app.config.from_object(DevConfig)
+    return app
 
 
 app = Flask(__name__)
@@ -28,8 +29,8 @@ class tempData(db.Model):
 @app.route("/")
 def send_data():
     data = reversed(tempData.query.order_by(tempData.rDate.desc()).limit(10).all())
-    temp_list = [['Time', 'Temprature']]
+    temp_list = []
     for atom in data:
-        atoms = [atom.rDate, atom.temp]
+        atoms = {"date": atom.rDate.strftime("%a, %d %b %Y %H:%M:%S"), "temp": atom.temp}
         temp_list.append(atoms)
     return render_template("index.html", temp_list=temp_list)
